22f01ecb31596bb430347d3f3cf5f67b73ab7264
Prithwis Das <prithwisdas2017@gmail.com>
2021-12-17 07:47:13 +0530

    update

--- main.py
+++ main.py
@@ -115,3 +115,46 @@ for presidents in list20_pres:
         writer_object = writer(f)
         writer_object.writerow(details20)
         f.close()
+
+#creating a list for 21st century vice-presidents
+for title in soup21_vp.findAll('div', {"id":"mw-pages"}):
+    names_21 = title.find_all('li')
+list21_vp = []    
+for i in range(len(names_21)):
+    list21_vp.append(names_21[i].get_text())
+
+#details for 21st century vice-presidents     
+for vice_presidents in list21_vp:
+    data = requests.get("https://en.wikipedia.org/wiki/{}".format(vice_presidents)).text
+    soup = BeautifulSoup(data, 'lxml')
+    politician = soup.find('div', class_='fn').get_text()
+    politician_fullname = soup.find('div', class_="nickname").get_text()
+    birth_date = soup.find('span', class_='bday').get_text()
+    age = soup.find('span', class_="noprint ForceAgeToShow").get_text()[5:8]
+    data_for_birthplace = soup.find_all('span', class_='noprint')
+    full_data = data_for_birthplace[0].parent.get_text()
+    full_data.split()
+    indices = []
+    for i in range(len(full_data)):
+        if full_data[i] == ')':
+            indices.append(i)
+    required_index = indices[1] + 1
+    birthplace = full_data[required_index:]
+    text_list = ['Democratic', 'Republican']
+    parties = []
+    for text in text_list:
+        party_full = soup.find(lambda tag: tag.name == "a" and text in tag.text).text
+        parties.append(party_full)    
+    details21_vp = []
+    details21_vp.append(politician)
+    details21_vp.append(politician_fullname)
+    details21_vp.append(birth_date)
+    details21_vp.append(age)
+    details21_vp.append(birthplace)
+    details21_vp.append(parties)
+    details21_vp.append("Vice President")
+    details21_vp.append(21)
+    with open('details.csv', 'a') as f:
+        writer_object = writer(f)
+        writer_object.writerow(details21_vp)
+        f.close()      
